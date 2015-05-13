345acd08d3256dcc721c0043fd90c4a4abf4743c
Nicolas Brugneaux <nicolas.brugneaux@gmail.com>
2015-05-13 19:07:11 +0200

    removed comments

--- src/components/tear.js
+++ src/components/tear.js
@@ -38,26 +38,15 @@ export default class Tear extends DynamicActor
 
     render()
     {
-
-// var radgrad4 = ctx.createRadialGradient(2,10,50,5,12,5);
-// radgrad4.addColorStop( 0, '#00C9FF' );
-// radgrad4.addColorStop( 0.8, '#00B5E2' );
-// radgrad4.addColorStop( 1, 'rgba(228,199,0,0)' );
-//
-// // draw shapes
-// ctx.fillStyle = radgrad4;
-// ctx.fillRect( this._x, this._y, this.sizeX, this.sizeY );
-    var gradient = ctx.createRadialGradient( this.center.x, this.center.y, 0, this.center.x, this.center.y, this.sizeX / 2 );
-    gradient.addColorStop( 0,'#0099FF' );
-    gradient.addColorStop( 1,'#00ABEB' );
-    // ctx.fillStyle = gradient;
-    // ctx.fillRect(this._x,this._y,200,200);
+        const colour = ctx.createRadialGradient( this.center.x, this.center.y, 0, this.center.x, this.center.y, this.sizeX / 2 );
+        colour.addColorStop( 0,'#0099FF' );
+        colour.addColorStop( 1,'#00ABEB' );
 
         ctx.beginPath();
         ctx.ellipse( this.center.x, this.center.y, this.sizeX / 2, this.sizeX / 2, 0, 0, Math.PI * 2 );
         ctx.closePath();
 
-        ctx.fillStyle = gradient;
+        ctx.fillStyle = colour;
         ctx.fill();
     }
 }
